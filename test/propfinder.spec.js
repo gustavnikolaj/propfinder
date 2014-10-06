0b0348a643555e9989ab795a87476f4cad5a2a91
Gustav Nikolaj Olsen <gno@one.com>
2014-10-06 10:39:54 +0200

    reverse the arguments, so that the path is first

--- test/propfinder.spec.js
+++ test/propfinder.spec.js
@@ -5,23 +5,23 @@ var propfinder = require('../lib/propfinder');
 describe('propfinder', function () {
 
     it('should return undefined for a property that does not exist', function () {
-        expect(propfinder({}, 'foo.bar'), 'to be undefined');
+        expect(propfinder('foo.bar', {}), 'to be undefined');
     });
 
     it('should return the property mentioned', function () {
-        expect(propfinder({ foo: 'bar' }, 'foo'), 'to be', 'bar');
+        expect(propfinder('foo', { foo: 'bar' }), 'to be', 'bar');
     });
 
     it('should return the property mentioned (nested)', function () {
-        expect(propfinder({ foo: { bar: 'baz' } }, 'foo.bar'), 'to be', 'baz');
+        expect(propfinder('foo.bar', { foo: { bar: 'baz' } }), 'to be', 'baz');
     });
 
     it('should return the property mentioned (more nested)', function () {
-        expect(propfinder({ foo: { bar: { baz: 'qux' } } }, 'foo.bar.baz'), 'to be', 'qux');
+        expect(propfinder('foo.bar.baz', { foo: { bar: { baz: 'qux' } } }), 'to be', 'qux');
     });
 
     it('should return undefined for a property that does not exist (nested)', function () {
-        expect(propfinder({ foo: {} }, 'foo.bar.qux'), 'to be undefined');
+        expect(propfinder('foo.bar.qux', { foo: {} }), 'to be undefined');
     });
 
 });
