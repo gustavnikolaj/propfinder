3f283bcc14657a06e1e46c58137146c75bf36ac1
Gustav Nikolaj Olsen <gno@one.com>
2014-10-06 10:50:09 +0200

    make it curryable and make it behave well for bad input

--- test/propfinder.spec.js
+++ test/propfinder.spec.js
@@ -24,4 +24,31 @@ describe('propfinder', function () {
         expect(propfinder('foo.bar.qux', { foo: {} }), 'to be undefined');
     });
 
+    it('should be curryable, return a function that finds a prop', function () {
+        var findFoo = propfinder('foo');
+        expect(findFoo, 'to be a function');
+        expect(findFoo({}), 'to be undefined');
+        expect(findFoo({ foo: true }), 'to be true');
+    });
+
+    it('should not return a method if obj is undefined', function () {
+        expect(propfinder('foo', undefined), 'to be undefined');
+    });
+
+    it('should return undefined when asked to find a property on a string', function () {
+        expect(propfinder('foo', 'bar'), 'to be undefined');
+    });
+
+    it('should return undefined when asked to find a property on null', function () {
+        expect(propfinder('foo', null), 'to be undefined');
+    });
+
+    it('should return undefined when asked to find a property on a number', function () {
+        expect(propfinder('foo', 1), 'to be undefined');
+    });
+
+    it('should return undefined when asked to find a property on an array', function () {
+        expect(propfinder('foo', [1, 2, 3]), 'to be undefined');
+    });
+
 });
